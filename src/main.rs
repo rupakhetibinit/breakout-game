86662f37dc8acb42dcea981acc13a9dbf83774b1
rupakhetibinit <crusierponder@protonmail.com>
2023-07-16 23:46:43 +0545

    add ball and spawning balls

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use macroquad::prelude::*;
 const PLAYER_SIZE: Vec2 = vec2(150f32, 40f32);
 const PLAYER_SPEED: f32 = 900f32;
 const BLOCK_SIZE: Vec2 = vec2(100f32, 40f32);
-
+const BALL_SIZE: f32 = 50f32;
+const BALL_SPEED: f32 = 400f32;
 struct Player {
     rect: Rect,
 }
@@ -61,10 +62,43 @@ impl Block {
     }
 }
 
+pub struct Ball {
+    rect: Rect,
+    vel: Vec2,
+}
+
+impl Ball {
+    pub fn new(pos: Vec2) -> Self {
+        Self {
+            rect: Rect::new(pos.x, pos.y, BALL_SIZE, BALL_SIZE),
+            vel: vec2(rand::gen_range(-1f32, 1f32), 1f32).normalize(),
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, GRAY)
+    }
+
+    pub fn update(&mut self, dt: f32) {
+        self.rect.x += self.vel.x * dt * BALL_SPEED;
+        self.rect.y += self.vel.y * dt * BALL_SPEED;
+        if self.rect.x < 0f32 {
+            self.vel.x = -1f32;
+        }
+        if self.rect.y < 0f32 {
+            self.vel.y = 1f32
+        }
+        if self.rect.x > screen_width() - self.rect.w {
+            self.vel.x = -1f32;
+        }
+    }
+}
+
 #[macroquad::main("Breakout Game")]
 async fn main() {
     let mut player = Player::new();
     let mut blocks: Vec<_> = Vec::new();
+    let mut balls = Vec::new();
 
     let (width, height) = (6, 6);
     let padding = 5f32;
@@ -79,13 +113,30 @@ async fn main() {
         let block_y = (i / width) as f32 * total_block_size.y;
         blocks.push(Block::new(board_start_pos + vec2(block_x, block_y)))
     }
+    balls.push(Ball::new(vec2(
+        screen_width() * 0.5f32,
+        screen_width() * 0.5f32,
+    )));
 
     loop {
+        if is_key_pressed(KeyCode::Space) {
+            balls.push(Ball::new(vec2(
+                screen_width() * 0.5f32,
+                screen_width() * 0.5f32,
+            )));
+        }
+
         player.update(get_frame_time());
+        for ball in balls.iter_mut() {
+            ball.update(get_frame_time());
+        }
         clear_background(WHITE);
         player.draw();
         for block in blocks.iter() {
             block.draw();
+        }
+        for ball in balls.iter() {
+            ball.draw();
         }
         next_frame().await
     }
