6a45ae8f4c24b044eb65b91ad7e3fac1be78f453
rupakhetibinit <crusierponder@protonmail.com>
2023-07-16 22:00:52 +0545

    add bricks struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use macroquad::prelude::*;
 
-const PLAYER_SIZE: Vec2 = vec2(100f32, 40f32);
-const PLAYER_SPEED: f32 = 700f32;
+const PLAYER_SIZE: Vec2 = vec2(150f32, 40f32);
+const PLAYER_SPEED: f32 = 900f32;
+const BLOCK_SIZE: Vec2 = vec2(100f32, 40f32);
 
 struct Player {
     rect: Rect,
@@ -20,7 +21,7 @@ impl Player {
     }
 
     pub fn draw(&self) {
-        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, DARKGRAY)
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, BLUE)
     }
 
     pub fn update(&mut self, dt: f32) {
@@ -41,13 +42,48 @@ impl Player {
     }
 }
 
-#[macroquad::main("Shooter Game")]
+struct Block {
+    rect: Rect,
+}
+
+impl Block {
+    pub fn new(pos: Vec2) -> Self {
+        Self {
+            rect: Rect::new(pos.x, pos.y, BLOCK_SIZE.x, BLOCK_SIZE.y),
+        }
+    }
+
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, DARKGRAY)
+    }
+}
+
+#[macroquad::main("Breakout Game")]
 async fn main() {
     let mut player = Player::new();
+    let mut blocks: Vec<_> = Vec::new();
+
+    let (width, height) = (6, 6);
+    let padding = 5f32;
+    let total_block_size = BLOCK_SIZE + vec2(padding, padding);
+    let board_start_pos = vec2(
+        (screen_width() - total_block_size.x * width as f32) * 0.5f32,
+        50f32,
+    );
+
+    for i in 0..width * height {
+        let block_x = (i % width) as f32 * total_block_size.x;
+        let block_y = (i / width) as f32 * total_block_size.y;
+        blocks.push(Block::new(board_start_pos + vec2(block_x, block_y)))
+    }
+
     loop {
         player.update(get_frame_time());
         clear_background(WHITE);
         player.draw();
+        for block in blocks.iter() {
+            block.draw();
+        }
         next_frame().await
     }
 }
